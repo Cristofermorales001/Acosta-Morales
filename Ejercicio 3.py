79af01b438162448abc2b856622f8deee21bdd01
Cristofermorales001 <c.morales@coadvil.edu.co>
2025-02-18 11:37:19 -0500

    Add files via upload

---
+++ Ejercicio 3.py
@@ -0,0 +1,5 @@
+radio=input("Ingrese el radio del circulo:" )
+perimetro=(2*float(radio))*3.1416
+area=3.1416*(float(radio)*float(radio))
+print("El perimetro del circulo es:",perimetro)
+print("El area del circulo es:",area)
---
+++ Ejercicio 4.py
@@ -0,0 +1,6 @@
+nun1 = int(input("Digite un número: "))
+nun2 = int(input("Digite el siguiente número: "))
+nun3 = int(input("Digite el siguiente número: "))
+nun4 = int(input("Digite el siguiente número: "))
+promedio = (nun1 + nun2 + nun3 + nun4) / 4
+print("El promedio es: ", promedio)
